46440b98b89304c852c242135a93a1c0d5a45eaf
rahul.lama <rahul.lama@DOTNET-RAHUL>
2023-02-01 10:14:18 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrimeCollege.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeCollege.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public List<Enrollment> Enrollments { get; set; }
+    }
+}
